6f9ad8e7f2a7f15317e755efb23aacb78563eac7
Nikita <nikki-xit@mail.ru>
2022-09-23 20:19:54 +0300

    create struct project

---
+++ src/app/NavigatorPageComponent/navigatorPage.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'NavigatorPageComponent',
+    templateUrl: './navigatorPage.component.html',
+    styleUrls: ['./navigatorPage.component.scss'],
+})
+export class NavigatorPageComponent {}
