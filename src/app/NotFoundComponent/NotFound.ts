1048f835e095796d2cad9a5f27045c040738fc3e
Nikita <nikki-xit@mail.ru>
2022-09-24 08:38:28 +0300

    creating home pages and update struct

--- src/app/NotFoundComponent/NotFound.ts
+++
@@ -1,8 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-    selector: 'NotFound',
-    templateUrl: './NotFound.component.html',
-    styleUrls: ['./NotFound.component.scss'],
-})
-export class NotFoundComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+import { NavigatePathComponent } from './NavigateProjectComponent/navigatePath.component';
 import { AppComponent } from './AppComponent/app.component';
 import { NavigatorPageComponent } from './NavigatorPageComponent/navigatorPage.component';
-import { NotFoundComponent } from './NotFoundComponent/NotFound';
+import { NotFoundComponent } from './NotFoundComponent/NotFound.component';
+
+const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: 'navigator', component: NavigatorPageComponent },
+    { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
-    declarations: [AppComponent, NavigatorPageComponent, NotFoundComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [AppComponent, NavigatorPageComponent, NotFoundComponent, NavigatePathComponent],
+    imports: [BrowserModule, RouterModule.forRoot(routes)],
     providers: [],
-    bootstrap: [AppComponent],
+    bootstrap: [NavigatePathComponent],
 })
 export class AppModule {}
