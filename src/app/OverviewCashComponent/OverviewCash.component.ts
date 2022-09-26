4d53e110524a2946e5ff9b7dd81d248f95bcf069
Nikita <nikki-xit@mail.ru>
2022-09-26 09:15:23 +0300

    update name-files in project

---
+++ src/app/OverviewCashComponent/OverviewCash.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'OverviewCashComponent',
+    templateUrl: './OverviewCash.component.html',
+    styleUrls: ['./OverviewCash.component.scss'],
+})
+export class OverviewCashComponent {}
